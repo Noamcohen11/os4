e0d0372250e86a86b7961b16b328a5f70b24a5e3
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-07-19 14:19:01 +0300

    auto commit

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -126,9 +126,9 @@ Victim __DFS(word_t base_address, word_t root = 0, int depth = 0, uint64_t paren
             if ((max_distance_address = 0) || (__get_cylindrical_distance(base_address, max_distance_address) <
                                                __get_cylindrical_distance(base_address, curr_table.longestDistnaceAddress)))
             {
-                std::cout << "max_distance_address: " << max_distance_address << " newParentAddress: " << newParentAddress << std::endl;
                 max_distance_address = curr_table.longestDistnaceAddress;
                 newParentAddress = (uint64_t)root * PAGE_SIZE + i;
+                std::cout << "max_distance_address: " << max_distance_address << " newParentAddress: " << newParentAddress << std::endl;
             }
             max_frame_address = MAX(MAX(max_frame_address, curr_table.maxFrame), new_root);
         }
