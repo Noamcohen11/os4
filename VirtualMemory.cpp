fb2db2dd027afa6ac02108b07fd56a076a6ca5ec
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-07-23 18:42:18 +0300

    auto commit

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -112,15 +112,14 @@ Victim __DFS(word_t base_pa, u_int64_t base_va, word_t root = 0, int depth = 0, 
         PMread((uint64_t)root * PAGE_SIZE + i, &new_root);
         if (new_root != 0)
         {
-            std::cout << "new root is not zero" << std::endl;
             empty = false;
             curr_table = __DFS(base_pa, base_va, new_root, depth + 1, (uint64_t)root * PAGE_SIZE + i, (virtualAddress * PAGE_SIZE) + i);
             if (curr_table.emptyAddress != 0)
             {
                 return curr_table;
             }
-            if ((max_distance_pa == 0) || (__get_cylindrical_distance(base_va, max_distance_va) <
-                                           __get_cylindrical_distance(base_va, curr_table.evicted_va)))
+            if ((curr_table.evicted_pa != 0) && ((max_distance_pa == 0) || (__get_cylindrical_distance(base_va, max_distance_va) <
+                                                                            __get_cylindrical_distance(base_va, curr_table.evicted_va))))
             {
 
                 max_distance_va = curr_table.evicted_va;
