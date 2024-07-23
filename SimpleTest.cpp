d0abce22bbe74834ebdd632dd06cae29f2486536
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-07-23 17:47:37 +0300

    auto commit

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -3,17 +3,21 @@
 #include <cstdio>
 #include <cassert>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     VMinitialize();
-    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-        printf("writing to %llu\n", (long long int) i);
+    printf("NUM_FRAMES %llu \n ", NUM_FRAMES);
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i)
+    {
+        printf("writing to %llu\n", (long long int)i);
         VMwrite(5 * i * PAGE_SIZE, i);
     }
 
-    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i)
+    {
         word_t value;
         VMread(5 * i * PAGE_SIZE, &value);
-        printf("reading from %llu %d\n", (long long int) i, value);
+        printf("reading from %llu %d\n", (long long int)i, value);
         assert(uint64_t(value) == i);
     }
     printf("success\n");
