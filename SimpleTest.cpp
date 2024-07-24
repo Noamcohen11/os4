8cd2d6019ac20ceb933506d467a547476ae317bb
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2024-07-24 14:31:58 +0300

    auto commit

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -9,13 +9,13 @@ int main(int argc, char **argv)
     for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i)
     {
         printf("writing to %llu\n", (long long int)i);
-        VMwrite(1 * i * 1, i);
+        VMwrite(5 * i * 1, i);
     }
 
     for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i)
     {
         word_t value;
-        VMread(1 * i * 1, &value);
+        VMread(5 * i * 1, &value);
         printf("reading from %llu %d\n", (long long int)i, value);
         assert(uint64_t(value) == i);
     }
